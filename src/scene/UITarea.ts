76a1b8bb13d0eb7860facf2cbf1d4d9988f91c84
martinCdia <forissimartin@gmail.com>
2024-03-11 02:25:30 -0300

    Se creo el container uidDialog

--- src/scene/UITarea.ts
+++ src/scene/UITarea.ts
@@ -9,6 +9,11 @@ export class UITarea extends Container {
     constructor() {
         super();
 
+        const uiDialog = new Container();
+        uiDialog.scale.set(0.8);
+        uiDialog.position.set(200,100);
+
+
         const background: NineSlicePlane = new NineSlicePlane(
             Texture.from("Box"),
             0,0,0,0
@@ -17,33 +22,35 @@ export class UITarea extends Container {
         background.height = 200;
         background.scale.set(3);
         background.position.set(550,100);
-        this.addChild(background);
+        uiDialog.addChild(background);
 
         const myUiBanner: UIBanner = new UIBanner();
         myUiBanner.position.set(620,-5);
-        this.addChild(myUiBanner);
+        uiDialog.addChild(myUiBanner);
 
         const myButtonClose: Sprite = Sprite.from("ButtonX");
         myButtonClose.scale.set(0.3);
         myButtonClose.position.set(1100,90)
-        this.addChild(myButtonClose);
+        uiDialog.addChild(myButtonClose);
 
         const myStarComplete: StarComplete = new StarComplete();
         myStarComplete.position.set(620,200);
-        this.addChild(myStarComplete);
+        uiDialog.addChild(myStarComplete);
 
         const myUiCoin:  UICoin = new UICoin();
         myUiCoin.position.set(622,410);
-        this.addChild(myUiCoin);
+        uiDialog.addChild(myUiCoin);
 
         const myUiDiamond: UIDiamond = new UIDiamond();
         myUiDiamond.position.set(865,410);
-        this.addChild(myUiDiamond);
+        uiDialog.addChild(myUiDiamond);
 
         const myButtonReplay: Sprite = Sprite.from("ButtonReplay");
         myButtonReplay.scale.set(0.4);
         myButtonReplay.position.set(660,510);
-        this.addChild(myButtonReplay);
+        uiDialog.addChild(myButtonReplay);
 
+        this.addChild(uiDialog);
+        
     };
 };
